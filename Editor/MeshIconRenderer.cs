6b6cafc9663d8afc66c902bd4ff854cee0663131
Denis Zdorovtsov <mrtrizer@gmail.com>
2025-06-18 21:29:12 +0300

    Minor fix

--- Editor/MeshIconRenderer.cs
+++ Editor/MeshIconRenderer.cs
@@ -29,7 +29,7 @@ namespace SiegeUp.IconRenderer.Editor
                                 material = meshRenderer.sharedMaterials[materialIndex];
                             var tweakedMaterial = iconInfo.renderConfig.GetMaterialReplacement(material);
 
-                            var matrix = Matrix4x4.TRS(iconInfo.renderConfig.Position, Quaternion.Euler(iconInfo.renderConfig.Rotation), iconInfo.renderConfig.Scale * 0.3f);
+                            var matrix = Matrix4x4.TRS(iconInfo.renderConfig.Position, Quaternion.Euler(iconInfo.renderConfig.Rotation), iconInfo.renderConfig.Scale);
                             var positionOffset = Matrix4x4.Translate(-iconInfo.renderConfig.Position);
                             preview.DrawMesh(meshFilter.sharedMesh, matrix * positionOffset * meshFilter.transform.localToWorldMatrix, tweakedMaterial, i);
                         }
